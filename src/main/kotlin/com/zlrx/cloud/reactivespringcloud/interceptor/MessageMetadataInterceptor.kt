45a3abb37f94000755f4b5129d0dbd47174612b5
zlaval <zlaval@gmail.com>
2021-05-06 15:02:10 +0200

    feat: add message metadata

--- src/main/kotlin/com/zlrx/cloud/reactivespringcloud/interceptor/MessageMetadataInterceptor.kt
+++ src/main/kotlin/com/zlrx/cloud/reactivespringcloud/interceptor/MessageMetadataInterceptor.kt
@@ -1,7 +1,9 @@
 package com.zlrx.cloud.reactivespringcloud.interceptor
 
+import org.apache.kafka.clients.producer.RecordMetadata
 import org.springframework.context.annotation.Configuration
 import org.springframework.integration.annotation.ServiceActivator
+import org.springframework.kafka.support.KafkaHeaders
 import org.springframework.messaging.Message
 
 @Configuration
@@ -9,6 +11,9 @@ class MessageMetadataInterceptor {
 
     @ServiceActivator(inputChannel = "producerMetadata")
     fun producerMetadata(msg: Message<*>) {
-        print(msg)
+        val metaData = msg.headers.get(KafkaHeaders.RECORD_METADATA, RecordMetadata::class.java)
+        if (metaData != null) {
+            println("Message was sent to ${metaData.topic()}. Partition: ${metaData.partition()} offset: ${metaData.offset()}")
+        }
     }
 }
